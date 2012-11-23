3ff2ee830bb3674a30322b6d5467c434d33108db
Matt Palmer <palmer.s.matt@gmail.com>
2012-11-23 18:17:58 -0500

    create main.go as distinct from ifbyphone.go

--- ifbyphone.go
+++ ifbyphone.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"bytes"
-	"fmt"
 	"io"
 	"io/ioutil"
 	"net/http"
@@ -10,14 +9,6 @@ import (
 
 const (
 	APIUrl = "https://secure.ifbyphone.com/ibp_api.php?"
-)
-
-var (
-	filterCriteria = map[string](func(CallRecord) bool){
-		"customer care":   func(cr CallRecord) bool { return cr.IsCustomerCare },
-		"answered":        func(cr CallRecord) bool { return !cr.IsMissed },
-		"during business": func(cr CallRecord) bool { return cr.IsMissed },
-	}
 )
 
 func callReader(start, end string) (io.Reader, error) {
@@ -34,40 +25,4 @@ func callReader(start, end string) (io.Reader, error) {
 		return nil, err
 	}
 	return bytes.NewBuffer(body), nil
-}
-
-func main() {
-	err := handleArgs()
-	if err != nil {
-		fmt.Printf("%v\n", err)
-		return
-	}
-
-	// The output of this function depends on the toFile flag.
-	calls, err := getCallsByDate(start, end)
-	if err != nil {
-		fmt.Printf("%v\n", err)
-		return
-	}
-
-	calls = Filter(calls, filterCriteria["customer care"])
-
-	var data CallGraph
-
-	switch *graphType {
-	case "duration":
-		data = GraphByDuration(calls)
-	case "agent":
-		data = GraphByAgent(calls)
-	case "hour":
-		data = GraphByHour(calls)
-	default:
-		fmt.Printf("invalid graphType specifed: %v", *graphType)
-		return
-	}
-
-	if err := graphOutput(data); err != nil {
-		fmt.Printf("%v\n", err)
-		return
-	}
 }
---
+++ main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+)
+
+var (
+	filterCriteria = map[string](func(CallRecord) bool){
+		"customer care":   func(cr CallRecord) bool { return cr.IsCustomerCare },
+		"answered":        func(cr CallRecord) bool { return !cr.IsMissed },
+		"during business": func(cr CallRecord) bool { return cr.IsMissed },
+	}
+)
+
+func main() {
+	err := handleArgs()
+	if err != nil {
+		fmt.Printf("%v\n", err)
+		return
+	}
+
+	// The output of this function depends on the toFile flag.
+	calls, err := getCallsByDate(start, end)
+	if err != nil {
+		fmt.Printf("%v\n", err)
+		return
+	}
+
+	calls = Filter(calls, filterCriteria["customer care"])
+
+	var data CallGraph
+
+	switch *graphType {
+	case "duration":
+		data = GraphByDuration(calls)
+	case "agent":
+		data = GraphByAgent(calls)
+	case "hour":
+		data = GraphByHour(calls)
+	default:
+		fmt.Printf("invalid graphType specifed: %v", *graphType)
+		return
+	}
+
+	if err := graphOutput(data); err != nil {
+		fmt.Printf("%v\n", err)
+		return
+	}
+}
