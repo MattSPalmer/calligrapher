b7fd9b56ce363109c435e3b9c87322231ab1f861
Matt Palmer <palmer.s.matt@gmail.com>
2012-11-24 12:54:05 -0500

    Add option to break up graph output by date.

--- output.go
+++ output.go
@@ -10,7 +10,9 @@ const (
 	extension = "csv"
 )
 
-func getCallsByDate(start, end string) ([]CallRecord, error) {
+// GetCallsByDate takes two date strings of the format "YYYYMMDD" and returns,
+// if successful, a slice of CallRecords.
+func GetCallsByDate(start, end string) ([]CallRecord, error) {
 	r, err := callReader(start, end)
 	if err != nil {
 		return nil, err
@@ -27,11 +29,52 @@ func getCallsByDate(start, end string) ([]CallRecord, error) {
 	if err != nil {
 		return nil, err
 	}
+
 	return calls, nil
 }
 
-func graphOutput(data CallGraph) error {
-	if *toFile {
+func rangeIntoDays(start, end string) ([]timeBlock, error) {
+	sT, err := time.Parse("20060102", start)
+	if err != nil {
+		return nil, err
+	}
+
+	eT, err := time.Parse("20060102", end)
+	if err != nil {
+		return nil, err
+	}
+
+	days := int(eT.Sub(sT).Hours())/24 + 1
+	dates := make([]timeBlock, days)
+	for i := 0; i < days; i++ {
+		durString := fmt.Sprintf("%dh", 24*i)
+		d, err := time.ParseDuration(durString)
+		if err != nil {
+			return nil, err
+		}
+
+		dayBegin := sT.Add(d)
+		dayEnd := dayBegin.Add(time.Duration(86399000000000))
+		dates[i] = timeBlock{dayBegin, dayEnd}
+	}
+	return dates, nil
+}
+
+func graphOutput(calls []CallRecord, graphType string, toFile, byDate bool) error {
+	var data CallGraph
+
+	switch graphType {
+	case "duration":
+		data = GraphByDuration(calls)
+	case "agent":
+		data = GraphByAgent(calls)
+	case "hour":
+		data = GraphByHour(calls)
+	default:
+		return fmt.Errorf("invalid graphType specifed: %v", graphType)
+	}
+
+	if toFile {
 		ds := time.Now().Format("01-02-06_15:04:05")
 		filePath := fmt.Sprintf("call_graph_%v.%v", ds, extension)
 		err := WriteToCSV(data, filePath)
