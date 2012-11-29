841d84eb32a47605bce6f06fa200876299c02d69
Matt Palmer <palmer.s.matt@gmail.com>
2012-11-29 01:03:36 -0500

    Fix merge oversight

--- flags.go
+++ flags.go
@@ -12,7 +12,6 @@ var (
 
 	graphType  = flag.String("g", "hour", "graph by agent, duration or call time")
 	byDate     = flag.Bool("d", false, "split data into separate days")
-	toFile     = flag.Bool("f", false, "write results to a CSV file")
 	toCSV      = flag.Bool("f", false, "write results to a CSV file")
 	toSVG      = flag.Bool("s", false, "create a graph in SVG format")
 
