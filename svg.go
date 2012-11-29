733f4ebfe1104ec76585d15cf20c750a04940c78
Matt Palmer <palmer.s.matt@gmail.com>
2012-11-29 00:49:10 -0500

    Add functions to graph calls over time

--- svg.go
+++ svg.go
@@ -35,3 +35,47 @@ func barGraph(cg CallGraph, fp string) error {
 
 	return nil
 }
+
+func durationStackGraph(w, h int, m map[string][]timeBlock, fp string) error {
+	f, err := os.Create(fp)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	canvas := svg.New(f)
+	canvas.Start(w, h)
+	defer canvas.End()
+
+	rh := (h - (10*len(m) + 60)) / len(m)
+	i := 30
+	for _, blocks := range m {
+		for _, b := range blocks {
+			start := w * (60*b.start.Hour() + b.start.Minute() - 540) / 660
+			pixelW := w * int(b.Duration().Minutes()) / 660
+			canvas.Rect(start, i-rh/2, pixelW, rh)
+		}
+		i += rh + 10
+	}
+	return nil
+}
+
+func mapCallTime(calls []CallRecord) (map[string][]timeBlock, error) {
+	m := make(map[string][]timeBlock)
+	dist, err := GraphByAgent(calls).Distribution()
+	if err != nil {
+		return nil, err
+	}
+	for k, v := range dist {
+		if k == -1 {
+			continue
+		}
+		b := make([]timeBlock, 0)
+		for _, call := range v {
+			b = append(b, *call.Interval())
+		}
+		name := agentsByID[k]
+		m[name] = b
+	}
+	return m, nil
+}
